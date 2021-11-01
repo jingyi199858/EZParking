6e0202b51feeb58e8b583afbd6435031a631aa80
jing <jingdakai@gmail.com>
2021-11-01 16:32:40 -0400

    Completed Register/Login system

--- nodeProject/frontend/src/App.js
+++ nodeProject/frontend/src/App.js
@@ -1,28 +1,31 @@
 import * as React from 'react';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import ReactMapGL, {Marker, Popup, GeolocateControl} from 'react-map-gl';
 import {Room, Star} from "@material-ui/icons"
 import "./app.css";
 import axios from "axios";
 import {format} from "timeago.js"
+import Register from './components/Register';
+import Login from './components/Login'
 
 function App() {
-  const currentUser = "James";
+  const myStorage = window.localStorage;
+  const [currentUser, setCurrentUser] = useState(myStorage.getItem("user"));
   const [pins,setPins] = useState([]);
   const [currentPlaceId, setCurrentPlaceId] = useState(null);
   const [newPlace, setNewPlace] = useState(null);
   const [title, setTitle] = useState(null);
   const [desc, setDesc] = useState(null);
   const [rating, setRating] = useState(0);
+  const [showRegister, setShowRegister] = useState(false);
+  const [showLogin, setShowLogin] = useState(false);
   const [viewport, setViewport] = useState({
-    width: "100vw",
-    height: "100vh",
     latitude: 40.74454886832962,
     longitude: -74.02553887611165,
     zoom: 14
   });
 
-  React.useEffect(()=>{
+  useEffect(()=>{
     const getPins = async ()=>{
       try {
         const res = await axios.get("/pins");
@@ -58,7 +61,7 @@ function App() {
       lat: newPlace.lat,
       long: newPlace.long,
 
-    }
+    };
 
     try {
       const res = await axios.post("/pins", newPin)
@@ -67,24 +70,31 @@ function App() {
     } catch (error) {
       console.log(error)
     }
-  }
+  };
+
+  const handleLogout = ()=>{
+    myStorage.removeItem("user");
+    setCurrentUser(null);
+  };
 
 
 
   return (
-    <div className="App">
+    <div style={{ height: "100vh", width: "100%" }}>
       <ReactMapGL
       {...viewport}
       mapboxApiAccessToken={process.env.REACT_APP_MAPBOX}
+      width="100%"
+      height="100%"
       onViewportChange={nextViewport => setViewport(nextViewport)}
       mapStyle="mapbox://styles/mapbox/streets-v11"
-      onClick = {handleAddClick}
-      dragPan="false"
-      touchAction="pan-y"
+      onDblClick = {handleAddClick}
+      // dragPan="false"
+      // touchAction="pan-y"
     >
 
       {pins.map(p=>(
-<>
+    <>
       
       <Marker latitude={p.lat} longitude={p.long} offsetLeft={-25.35} offsetTop={-45}>
         <Room style={{ fontSize: viewport.zoom * 3, color: p.username === currentUser ? "tomato" :"slateblue",
@@ -93,7 +103,7 @@ function App() {
         onClick={()=>handleMarkerClick(p._id, p.lat, p.long)}
         />
       </Marker>
-      {p._id === currentPlaceId &&
+      {p._id === currentPlaceId &&(
       <Popup
           latitude={p.lat}
           longitude={p.long}
@@ -116,10 +126,10 @@ function App() {
             <span className="date">{format(p.createdAt)}</span>
           </div>
         </Popup>
-        } 
+      )} 
         </>
         ))}
-        {newPlace &&
+        {newPlace && (
         <Popup
           latitude={newPlace.lat}
           longitude={newPlace.long}
@@ -146,7 +156,21 @@ function App() {
           </form>
         </div>
           </Popup>
-          }
+        )}
+        {currentUser ? (<button className="button logout" onClick={handleLogout}>Log out</button>) : (
+          <div className="buttons">
+            <button className="button login" onClick={() => setShowLogin(true)}>Log in</button>
+            <button className="button register" onClick={() => setShowRegister(true)}>Register</button>
+          </div>
+        )}
+        {showRegister && <Register setShowRegister={setShowRegister} />}
+        {showLogin && (
+        <Login 
+          setShowLogin={setShowLogin} 
+          myStorage={myStorage}
+          setCurrentUser={setCurrentUser}
+        />
+        )}
     </ReactMapGL>
     </div>
   );
